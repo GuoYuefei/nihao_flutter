cd948fe5190634cfb9fbd18d35e2a9b93e343cce
Solomon Guo <beconegood@gmail.com>
2018-12-19 11:56:58 +0800

    完工

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,9 @@ class MyApp extends StatelessWidget {
           child: new RandomWords(),
         ),
       ),
+      theme: new ThemeData(
+        primaryColor: Colors.indigo,
+      ),
     );
   }
 }
@@ -28,6 +31,8 @@ class RandomWords extends StatefulWidget {
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggrstions = new List<WordPair>();
 
+  final Set<WordPair> _saved = new Set<WordPair>();
+
   final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
@@ -38,17 +43,66 @@ class RandomWordsState extends State<RandomWords> {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
 
+  //生成导航条
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            }
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
+      ),
+    );
+    
+  }
+
   Widget _buildRow(WordPair pair) {
+    final alreadtSaved = _saved.contains(pair);
     return new ListTile (
       title: new Text(
-        pair.asCamelCase,
+        pair.asPascalCase,
         style: _biggerFont
       ),
+      trailing: new Icon(
+        alreadtSaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadtSaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+                  if(alreadtSaved) {
+                    _saved.remove(pair);
+                  } else {
+                    _saved.add(pair);
+                  }
+                });
+      },
     );
   }
 
@@ -61,6 +115,7 @@ class RandomWordsState extends State<RandomWords> {
 
         final index = i ~/ 2;
 
+        //如果封底了，那么就在生成10个WordPair
         if(index >= _suggrstions.length) {
           _suggrstions.addAll(generateWordPairs().take(10));
         }
